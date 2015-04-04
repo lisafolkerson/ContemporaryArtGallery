2dc96605e679a624c4c270245cbafe0f3b4d9535
Lisa Folkerson <lisafolkerson@gmail.com>
2015-04-04 10:26:39 -0600

    dates to exhibitions

--- wp-content/themes/contemporary_art_gallery/exhibitions-loop.php
+++ wp-content/themes/contemporary_art_gallery/exhibitions-loop.php
@@ -11,6 +11,7 @@
 
       <h2 data-img="<?php echo $url; ?>"><a href="#"><?php the_title(); ?></a></h2><?php the_content(); ?>
       <?php //$title= get_the_title() ?>
+      the dates
             
       <?php endwhile; // end the loop?>
       <?php wp_reset_postdata(); ?>
