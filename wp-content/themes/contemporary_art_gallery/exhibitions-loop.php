d192a5decf50dfe8934462c5b278ba4c58533d13
Lisa Folkerson <lisafolkerson@gmail.com>
2015-04-13 13:16:55 -0400

    single-exhibition template added

--- wp-content/themes/contemporary_art_gallery/exhibitions-loop.php
+++ wp-content/themes/contemporary_art_gallery/exhibitions-loop.php
@@ -8,14 +8,16 @@
       <?php $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large', true); ?>
 
       <img src="<?php echo $url ?>" style="display:none;" alt="">
+      <div class="singlePostEx">
       <!-- name of the exhibition -->
-      <h2 data-img="<?php echo $url; ?>"><a href="#"><?php the_title(); ?></a></h2><?php the_content(); ?><br>
-      
-      <!-- start and end dates of the exhibition -->
-      <?php if( get_field('date_start') ): ?>
-        <p><?php the_field('date_start'); ?> - 
-        <?php the_field('date_end') ?></p>
-      <?php endif; ?>
+        <h2 data-img="<?php echo $url; ?>"><a href="#"><?php the_title(); ?></a></h2><?php the_content(); ?><br>
+        
+        <!-- start and end dates of the exhibition -->
+        <?php if( get_field('date_start') ): ?>
+          <p class="date"><?php the_field('date_start'); ?> - 
+          <?php the_field('date_end') ?></p>
+        <?php endif; ?>
+      </div><!--end .singlePostEx-->
             
       <?php endwhile; // end the loop?>
       <?php wp_reset_postdata(); ?>
